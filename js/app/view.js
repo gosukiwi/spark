e41ae49f32a9960b81de54fab1beee655d59e53f
Federico Ramírez <fedekiler@gmail.com>
2013-11-01 18:05:28 -0300

    Views now use require to generate a URL

--- js/app/view.js
+++ js/app/view.js
@@ -1,16 +1,13 @@
 define(['jquery', 'hogan'], function ($) {
-    var cache = {},
-        baseUrl = 'js/app/views/';
+    var cache = {};
 
     return function (src, context) {
         var res,
-            path = baseUrl + src;
+            path = require.toUrl('app/views/' + src);
         
         if(!cache[src]) {
             jQuery.ajaxSetup({async:false});
-            $.get(path, function (data) {
-                res = data;
-            });
+            $.get(path, function (data) { res = data; });
             jQuery.ajaxSetup({async:true});
             cache[src] = Hogan.compile(res);
         }
