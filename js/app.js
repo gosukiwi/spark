5faa94f6a95b43158876fd35055342642b4755d9
Federico Ramírez <fedekiller@gmail.com>
2013-11-06 15:25:23 +0000

    Requirejs configuration fix

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 requirejs.config({
     //By default load any module IDs from js/lib
-    baseUrl: 'js/vendor',
+    baseUrl: 'js/',
     //except, if the module ID starts with "app",
     //load it from the js/app directory. paths
     //config is relative to the baseUrl, and
     //never includes a ".js" extension since
     //the paths config could be for a directory.
     paths: {
-        app: '../app',
-        jaf: '../jaf',
-        underscore: '../vendor/lodash'
+        'underscore': 'vendor/lodash',
+        'jquery': 'vendor/jquery',
+        'jquery-ui': 'vendor/jquery-ui',
+        'md5': 'vendor/md5',
+        'hogan': 'vendor/hogan',
+        'codemirror': 'vendor/codemirror',
     }
 });
 
