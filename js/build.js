8ea8f7d0924298fc6276e14d38bb39823c6df965
Federico Ramírez <fedekiller@gmail.com>
2013-11-06 15:47:33 +0000

    Add r.js configuration file for release builds

---
+++ js/build.js
@@ -0,0 +1,13 @@
+({
+    baseUrl: ".",
+    paths: {
+        'underscore': 'vendor/lodash',
+        'jquery': 'vendor/jquery',
+        'jquery-ui': 'vendor/jquery-ui',
+        'md5': 'vendor/md5',
+        'hogan': 'vendor/hogan',
+        'codemirror': 'vendor/codemirror',
+    },
+    name: "app",
+    out: "app-built.js"
+})
